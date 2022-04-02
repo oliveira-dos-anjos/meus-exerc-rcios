074f56d0d777363d0d7237f7e5cb87df7774f017
oliveira-dos-anjos <101524748+oliveira-dos-anjos@users.noreply.github.com>
2022-04-01 21:33:26 -0300

    Update funções.py

--- criando um menu e tratando erros/funções.py
+++ criando um menu e tratando erros/funções.py
@@ -25,7 +25,21 @@ def leiaInt2(msg):
         else:
             return n2
 
+        
+ def leiafloat(msg):
+    while True:
+        try:
+            nf = float(input(msg))
+        except (ValueError, TypeError):
+            print(f'\033[33mERRO: o dado não corresponde a um valor real')
+            continue
+        except KeyboardInterrupt:
+            print('\033[31mA leitura de dados foi interrompida pelo usuário')
+            return 0
+        else:
+            return nf
 
+        
 def dividir(n1, n2):
     try:
         r = n1 / n2
