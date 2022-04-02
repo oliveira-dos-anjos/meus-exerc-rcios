fbaa51078ed72c70435f48ea8e841524653978f6
oliveira-dos-anjos <101524748+oliveira-dos-anjos@users.noreply.github.com>
2022-04-02 14:15:18 -0300

    Delete mini tratanemto de dados com tratamento de  erros directory

--- mini tratanemto de dados com tratamento de  erros/programa principal.py
+++
@@ -1,6 +0,0 @@
-from funções import *
-
-ni = leiaInt('\033[32mDigite um valor inteiro: ')
-nf = leiafloat('\033[32mDigite um valor real: ')
-print(f"\033[34mO valor inteiro é {ni}")
-print(f'O valor real é {nf}\033[m')
